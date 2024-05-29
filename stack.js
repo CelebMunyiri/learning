8f463fd81e26a2f35412a9b454c634e23d43ce38
David Munyiri <david@nathandigital.com>
2024-05-29 15:05:23 +0300

    added a hackerrank problem and solution in the fizz.js file

--- stack.js
+++ stack.js
@@ -29,3 +29,17 @@ class Stack{
     }
 
 }
+
+let stack=new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+
+console.log(stack.size());
+stack.pop();
+
+console.log(stack.size());
+console.log(stack.isEmpty());
+console.log(stack.peek());
